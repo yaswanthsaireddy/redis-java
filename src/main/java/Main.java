3be9845b5dc93cf6d3f94f6bfa7434a4c23c4648
yaswanthsaireddy <yaswanthsaireddy88@gmail.com>
2025-12-23 22:32:41 +0530

    multiple responses

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,13 +12,22 @@ public class Main {
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
-          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+            // Get the input stream to read what the client sends
+            java.io.InputStream input = clientSocket.getInputStream();
+            byte[] buffer = new byte[1024];
+
+            // Keep reading while the client is sending data
+            while (input.read(buffer) != -1) {
+                // In this stage, we assume any input is a PING
+                // We respond with +PONG\r\n
+                clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+            }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
